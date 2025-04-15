8f7478f4983533d608e5f57f6498e8b61d041b4a
Sabbalingurinn <s.b.snorrason@gmail.com>
2025-04-15 21:08:05 +0000

    lokadæmi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ export default function HomePage() {
     <div>
       <h1 className="text-2xl font-semibold mb-4">Velkomin á Matarplanara!</h1>
       <p>Þú getur skoðað uppskriftir, búið til þínar eigin og haldið utan um hvað þú borðar.</p>
+      <p>Til að skoða notanda með uppskriftir skaltu skrá þig inn með:</p>
+      <p>Notendanafn: osk</p>
+      <p>Lykilorð: plsgemmer10</p>
     </div>
   );
 }
